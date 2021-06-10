a81f28f5debfb25ca58edb83ff9e53b6bd623771
Viktor Nyström <95viknys@skola.boras.se>
2021-06-10 19:59:21 +0200

    Showing day, date, month and time working

--- script/WelcomeSegment.js
+++ script/WelcomeSegment.js
@@ -1,21 +1,24 @@
 window.addEventListener('load', main);
 
 function main() {
-    //Tid
-    getTimeOfDay();
-    
-    //Veckodag
-    // getWeekDay();
+    printTimeOfDay();
 
-    //Datum
+    printDayDateAndMonth();
 }
 
-function getTimeOfDay() {
-    let currentTime = new Date()
-    let hours = currentTime.getHours()
-    let minutes = currentTime.getMinutes()
+function printDayDateAndMonth(params) {
+    let date = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let dateString = date.toLocaleDateString('sv', options);
+    document.getElementById('date').innerHTML = dateString;
+}
+
+function printTimeOfDay() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
     let seconds = currentTime.getSeconds();
-    
+
     if (minutes < 10) {
         minutes = '0' + minutes
     }
@@ -23,9 +26,9 @@ function getTimeOfDay() {
     if (seconds < 10) {
         seconds = '0' + seconds
     }
-    
+
     var t_str = hours + ':' + minutes + ':' + seconds;
-    
+
     document.getElementById('clock').innerHTML = t_str;
 }
-setInterval(getTimeOfDay, 500);
+setInterval(printTimeOfDay, 500);
