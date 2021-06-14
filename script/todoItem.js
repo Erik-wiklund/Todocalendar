ee432afde1aec69c7df3deed079e05938fbc628e
Rasmus Nygren <87rasnyg@skola.boras.se>
2021-06-14 14:09:19 +0200

    change todo done

--- script/todoItem.js
+++ script/todoItem.js
@@ -35,9 +35,11 @@ function fillTodoList() {
     for (let todo of todoDictionary[local_id]) {
         const todoList = document.querySelector(".todo-item-list");
         const newTodoDiv = document.createElement("div");
-        newTodoDiv.className = ("full-width" + " flex" + " space-around" + " no-margin-on-p");
+        newTodoDiv.className = ("full-width flex space-around no-margin-on-p");
 
-        const todoText = document.createElement("p").innerText = todo;
+        const todoText = document.createElement("p");
+        todoText.addEventListener("click", editTodo);
+        todoText.innerText = todo;
 
         const minusButton = document.createElement("p");
         minusButton.addEventListener("click", removeTodo);
@@ -51,12 +53,9 @@ function fillTodoList() {
 }
 
 function addNewTodo() {
-    let a = todoDictionary[local_id];
-    if (!a) {
+    if (!todoDictionary[local_id]) {
         todoDictionary[local_id] = [];
     }
-
-    //let add_id = local_id;
 
     const todo = prompt("skriv nytt todo");
     todoDictionary[local_id].push(todo);
@@ -70,14 +69,29 @@ function addNewTodo() {
 
 function removeTodo(event) {
 
-    let text = event.target.parentNode.childNodes[0].wholeText;
+    let text = event.target.parentNode.childNodes[0].innerText;
+    const index =  getStringIndex(text);
 
-    for (let i = 0; i < todoDictionary[local_id].length; i++) {
-        if (todoDictionary[local_id][i] === text) {
-            todoDictionary[local_id].splice(i, 1);
-            break;
-        }
+    if (index || index === 0){
+        todoDictionary[local_id].splice(index, 1);
     }
+
     createTodoList(local_id);
     createCalender();
+}
+
+function editTodo(event){
+    const text = event.target.innerText;
+    const index = getStringIndex(text);
+
+    if (index || index === 0) {
+        const newTodo = prompt("Ändra Todo", text);
+        todoDictionary[local_id][index] = newTodo;
+    }
+
+    createTodoList(local_id);
+}
+
+function getStringIndex(string){
+    return todoDictionary[local_id].findIndex(x => x === string);
 }
